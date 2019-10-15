158d71d8e2d164fdd608fa7200820976aca9f17b
Erik Sandoval <erik.sandoval@icloud.com>
2019-10-14 17:34:57 -0700

    added move to front and move to back.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -89,8 +89,8 @@ class DoublyLinkedList:
         self.length -= 1
         return old_node.value
 
-    """Wraps the given value in a ListNode and inserts it 
-    as the new tail of the list. Don't forget to handle 
+    """Wraps the given value in a ListNode and inserts it
+    as the new tail of the list. Don't forget to handle
     the old tail node's next pointer accordingly."""
 
     def add_to_tail(self, value):
@@ -105,7 +105,7 @@ class DoublyLinkedList:
         self.length += 1
         return self
 
-    """Removes the List's current tail node, making the 
+    """Removes the List's current tail node, making the
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
 
@@ -123,17 +123,27 @@ class DoublyLinkedList:
         self.length -= 1
         return old_node.value
 
-    """Removes the input node from its current spot in the 
+    """Removes the input node from its current spot in the
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
 
-    """Removes the input node from its current spot in the 
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
+
+    """Removes the input node from its current spot in the
     List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
 
     """
     Removes a node from the list and handles cases where
@@ -144,10 +154,10 @@ class DoublyLinkedList:
         self.length -= 1
         if node == self.head:
             self.remove_from_head()
-            return None
+
         elif node == self.tail:
             self.remove_from_tail()
-            return None
+
         else:
             node.delete()
 
